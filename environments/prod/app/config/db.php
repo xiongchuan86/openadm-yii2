7ed358c0dbfe9fd317b94ec064ca054e44bc5f55
xiongchuan <xiongchuan86@gmail.com>
2017-01-06 13:20:41 +0800

    配置prod的db

--- environments/prod/app/config/db.php
+++ environments/prod/app/config/db.php
@@ -4,9 +4,9 @@ $config = [
     'components' => [
         'db' => [
             'class' => 'yii\db\Connection',
-            'dsn' => 'mysql:host=127.0.0.1;dbname=openadm',
-            'username' => 'root',
-            'password' => '123456',
+            'dsn' => 'mysql:host=rm-2ze5bqwi8wy0w0190.mysql.rds.aliyuncs.com;dbname=db_openadm',
+            'username' => 'mysql_openadm',
+            'password' => 'LOOK@d3qu',
             'charset' => 'utf8',
             'tablePrefix' => 'oa_',
         ],
